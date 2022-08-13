0b4201873e0de85a7b7fd7e839ac37520bf4937a
oolufolabii <otitoola.olufolabi@gmail.com>
2022-08-13 10:54:24 +0100

    Sorting Algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,26 +9,26 @@
 void bubble_sort(int *array, size_t size)
 
 {
-    unsigned int a, b;
-    int temp;
+	unsigned int a, b;
+	int temp;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    for (a = 0; a < size - 1; a++)
-    {
+	for (a = 0; a < size - 1; a++)
+	{
 
-        for (b = 0; b < size - a - 1; b++)
-        {
+		for (b = 0; b < size - a - 1; b++)
+		{
 
-            if (array[b] > array[b + 1])
-            {
+			if (array[b] > array[b + 1])
+			{
 
-                temp = array[b];
-                array[b] = array[b + 1];
-                array[b + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+				temp = array[b];
+				array[b] = array[b + 1];
+				array[b + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
